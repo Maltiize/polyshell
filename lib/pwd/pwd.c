bc8c9bd1f04644d636f0cf4545f094470396bfe9
Romain CHARRETTEUR <romain.charretteur@et.univ-nantes.fr>
2016-03-27 10:30:12 +0200

    Ajout de l'option help dans pwd, pour tester getopt

--- lib/pwd/pwd.c
+++ lib/pwd/pwd.c
@@ -9,6 +9,38 @@
 // Fonction PWD return 0 si ca marche et print le chemin sinon return 0 et écrit qu'il y a un probleme
 int pwd(int argc, char *argv[])
 {
+    int help = 0; //option h
+    char opt; // Pour stocker les options passé en paramètre 1 à 1
+    char * options = "h" ; // Les options disponibles
+    // Récupérer les options
+    while ((opt = getopt (argc, argv, options)) != -1)
+    {
+        switch(opt)
+        {
+            case '?':
+                printf("Un des paramètres n'est pas reconnu. Utiliser -h pour obtenir de l'aide. \n");
+                return 1;
+                break;
+            case 'h':
+                help = 1;
+                break;
+            default:
+                printf("Une erreur est survenu lors de la saisie des paramètres. \n");
+                return 1;
+                break;
+        }
+    }
+
+    // Si -h
+    if (help == 1)
+    {
+        printf("Pour utiliser la fonction pwd taper : pwd [-options] \n");
+        printf("Affiche le nom du répertoire de travail actuel. \n");
+        printf("Astuce : vous pouvez concaténer les options : -l -a = -la \n \n");
+        printf("\t -h : Affiche l'aide. \n");
+        return 0;
+    }
+
     char buff[PATH_MAX + 1];    // On stock la taille du chemin max + 1
     char* path = getcwd( buff, PATH_MAX + 1 );
 

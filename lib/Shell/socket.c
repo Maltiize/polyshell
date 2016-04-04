87f18cace6c0f3aac75ba5eabe06712ed1b6e359
Raphael <raphael.barraud@hotmail.fr>
2016-04-04 11:51:13 +0000

    socket.c

--- lib/Shell/socket.c
+++ lib/Shell/socket.c
@@ -1,6 +1,17 @@
-SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
-if(sock == INVALID_SOCKET)
+#include <sys/types.h> /* Consultez NOTES */ 
+#include <sys/socket.h> 
+
+void connects(int ip,int port){
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+if(sock <= 0)
 {
     perror("socket()");
-    exit(errno);
+    exit(0);
+}
+int connect(sock,ip);
+    
+}
+int main(){
+    connects (20,1);
+    return 0;
 }
